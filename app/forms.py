6b586e750285da398661d4d1c2cea98118c6b34b
Kamyar Bashar <kamyar.bashar@gmail.com>
2024-02-09 20:30:15 +0330

    moved function from forms to views

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,4 @@
 from django import forms
 from .models import AuthUser
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = AuthUser
-        fields = ['username', 'password', 'email']
-    
-    def clean_password(self):
-        password = self.cleaned_data['password']
-        if len(password) < 6:
-            raise forms.ValidationError("Password must be at least 6 characters long.")
-        return password
+#will add if needed 
--- app/views.py
+++ app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.shortcuts import render
-from .forms import UserRegistrationForm
 
 # Create your views here.
 def index(request):
