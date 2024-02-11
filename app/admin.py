ff621e05dc445ef009d4377c90d70f38db2ccfe3
Kamyar Bashar <kamyar.bashar@gmail.com>
2024-02-11 21:42:12 +0330

    database improved

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from .models import News
+from .models import Featured
+from .models import Trending
+
 
 admin.site.register(Post)
+admin.site.register(News)
+admin.site.register(Featured)
+admin.site.register(Trending)
--- app/models.py
+++ app/models.py
@@ -24,3 +24,39 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+class Trending(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='static/post_images/', blank=True, null=True)
+    image1 = models.ImageField(upload_to='static/post_images/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='static/post_images/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
+class Featured(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='static/post_images/', blank=True, null=True)
+    image1 = models.ImageField(upload_to='static/post_images/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='static/post_images/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='static/post_images/', blank=True, null=True)
+    image1 = models.ImageField(upload_to='static/post_images/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='static/post_images/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
