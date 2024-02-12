687087a3900ce7f36d362b14c3ccab4012a76351
Kamyar Bashar <kamyar.bashar@gmail.com>
2024-02-12 14:02:12 +0330

    now login is now required

--- app/views.py
+++ app/views.py
@@ -10,70 +10,114 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.core.cache import cache
 from django.conf import settings
 from CAC.settings import LOGIN_ATTEMPTS_LIMIT, LOGIN_ATTEMPTS_TIMEOUT
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 def index(request):
     return render(request, 'index.html')
+
 def projects(request):
     return render(request, 'projects.html')
+
 def blog(request): 
     return render(request, 'blog.html')
+
 def contact(request): 
     return render(request, 'contact.html')
+
 def projects(request): 
     return render(request, 'projects.html')
+
 def purchase(request):
     return render(request, 'purchase.html')
+
 def timeline(request): 
     return render(request, 'timeline.html')
+
 def arcade(request): 
     return render(request, 'arcade.html')
+
 def payment(request): 
     return render(request, 'payment.html')
+
 def tetris(request): 
     return render(request, 'tetris.html')
+
 def snake(request): 
     return render(request, 'snake.html')
+
+@login_required()
 def password(request): 
     return render(request, 'password.html')
+
 def pong(request): 
     return render(request, 'pong.html')
+
 def signup(request): 
     return render(request, 'signup.html')
+
 def mario(request): 
     return render(request, 'mario.html')
+
 def login(request): 
     return render(request, 'login.html')
+
+@login_required()
 def forgetpass(request): 
     return render(request, 'forgetpass.html')
+
 def blogmenu(request):
     context = {
         'posts' : Post.objects.all()
     } 
     return render(request, 'blogmenu.html', context)
+
 def verification(request): 
     return render(request, 'verification.html')
+
+@login_required()
 def notepad(request): 
     return render(request, 'notepad.html')
+
+@login_required()
 def myip(request): 
     return render(request, 'myip.html')
+
+@login_required()
 def calculator(request): 
     return render(request, 'calculator.html')
+
 def arcadegames(request): 
     return render(request, 'arcadegames.html')
+
+@login_required()
 def profile(request): 
     return render(request, 'profile.html')
+
+@login_required()
 def camera(request): 
     return render(request, 'camera.html')
+
+@login_required()
 def internetspeedtest(request): 
     return render(request, 'internetspeedtest.html')
+
+@login_required()
 def resetpassword(request): 
     return render(request, 'resetpassword.html')
+
+@login_required()
 def tools(request): 
     return render(request, 'tools.html')
+
+@login_required()
 def passwordgenerator(request): 
     return render(request, 'passwordgenerator.html')
+
+@login_required()
 def settings(request): 
     return render(request, 'settings.html')
+
 def home(request):
     return redirect('/')
 
@@ -147,8 +191,8 @@ logger = logging.getLogger(__name__)
 
 def login(request):
     try:
+        next_url = request.GET.get('next', '/')  # Default to home page if 'next' parameter is not provided
         if request.method == 'POST':
-            # Rate limiting and logging
             ip_address = request.META.get('REMOTE_ADDR')
             login_attempts_key = f'login_attempts_{ip_address}'
             login_attempts = cache.get(login_attempts_key, 0)
@@ -167,22 +211,25 @@ def login(request):
                 user = authenticate(username=username, password=password)
                 if user is not None:
                     auth_login(request, user)
-                    # Reset login attempts on successful login
                     cache.delete(login_attempts_key)
                     logger.info(f'Successful login for user: {username}, IP: {ip_address}')
                     
                     messages.success(request, 'You have successfully logged in.')
-                    return redirect('/')
+                    
+                    next_url = request.POST.get('next')
+                    if next_url:
+                        return redirect(next_url)
+                    else:
+                        return redirect('/')   # Redirect to the home page if no next URL
                 else:
                     messages.error(request, 'Invalid username or password')
             else:
                 messages.error(request, 'Invalid username or password')
 
-            # Increment login attempts
-            cache.set(login_attempts_key, login_attempts + 1, timeout= LOGIN_ATTEMPTS_TIMEOUT)
+            cache.set(login_attempts_key, login_attempts + 1, timeout=LOGIN_ATTEMPTS_TIMEOUT)
         else:
             form = AuthenticationForm()
-        return render(request, 'login.html', {'form': form})
+        return render(request, 'login.html', {'form': form, 'next': next_url})
     except Exception as e:
         logger.error(f'An error occurred: {str(e)}')
         messages.error(request, 'An unexpected error occurred. Please try again later.')
